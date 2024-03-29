589a5be971adb92b97e34a587be2af455d148680
Olga Ilnitskya <coroniola@gmail.com>
2024-03-29 11:35:48 +0300

    added tests and README

---
+++ conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from main import BooksCollector
+
+@pytest.fixture
+def books_list():
+    collector = BooksCollector()
+    collector.add_new_book('Гордость и предубеждение и зомби')
+    collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    collector.add_new_book('Карлсон')
+    return collector
+
